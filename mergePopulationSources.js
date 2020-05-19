e9e6df3c02d0f51bc639d87921ed552727a261a0
trycrmr <trycrmr@gmail.com>
2020-05-19 09:51:56 -0400

    Adds US county and country populations

---
+++ mergePopulationSources.js
@@ -0,0 +1,4 @@
+const countyPops = require('./USCountyPopulationsJul12019.csv') // https://www.census.gov/data/tables/time-series/demo/popest/2010s-counties-total.html
+const countryPops = require('./CountryPopulationsJul12019.csv') // https://population.un.org/wpp/Download/Standard/Population/
+
+console.info(countryPops)
